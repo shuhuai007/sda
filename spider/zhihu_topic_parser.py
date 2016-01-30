0a3420cf4a2ae41af08f154aa890398b7743f8a8
Jie Zhou <zhoujie338@126.com>
2016-01-31 01:23:07 +0800

    refactor the topic parser

--- spider/zhihu_topic_parser.py
+++ spider/zhihu_topic_parser.py
@@ -14,18 +14,21 @@ import threading
 import Queue
 import ConfigParser
 
+from urllib import urlencode
+
 from zhihu_util import get_content
 from zhihu_util import post
+from zhihu_util import get_xsrf_from_cookie
+from zhihu_util import get_xsrf
+
 
 # 1st Level Topics url
 LEVEL1_TOPICS_URL = "https://www.zhihu.com/topics"
 # 2st Level Topics url
 LEVEL2_TOPICS_URL = "https://www.zhihu.com/node/TopicsPlazzaListV2"
 
-
 LEVER2_TOPIC_COUNT_PER_PAGE = 20
 LEVEL2_TOPIC_MAX_PAGE_INDEX = 1000
-
 
 
 def fetch_level1_topic_list(level1_topic_url=LEVEL1_TOPICS_URL):
@@ -63,9 +66,9 @@ def fetch_level2_topic_list(level1_list):
         offset = 0
         page_index = 1
         while page_index < LEVEL2_TOPIC_MAX_PAGE_INDEX:
-            content = post(topic_url, level1_topic_id, hash_id, offset)
+            # content = post(topic_url, level1_topic_id, hash_id, offset)
+            content = post(topic_url, generate_post_data_for_level2(hash_id, level1_topic_id, offset))
             # print "...level2 topic content:%s:" % content
-
             temp_list = parse_level2_response(content)
             if temp_list:
                 level2_topic_list += temp_list
@@ -114,4 +117,20 @@ def parse_level2_response(content):
         print "\n\nresult_list's len:%d" % len(result_list)
     return result_list
 
-
+def generate_post_data_for_level2(hash_id, level1_topic_id, offset):
+    post_dict = {}
+    post_dict["method"] = "next"
+    # post_dict["params"] = '{"topic_id":253,"offset":40,"hash_id":"dced108689287057f5cc3b5e85cb8289"}'
+    params_dict = '{' \
+                  '"topic_id":' + str(level1_topic_id) + ',' \
+                  '"offset":' + str(offset) + ',' \
+                  '"hash_id":' + '"' + str(hash_id) + '"' \
+                  '}'
+    post_dict["params"] = params_dict
+    print "\n\nparams_dict:%s" % params_dict
+    # post_dict["_xsrf"] = "dacc17fefe1dd92f1f814fb77d3a359f"
+    post_dict["_xsrf"] = get_xsrf()
+    # print "\n\n...xsrf:%s" % post_dict["_xsrf"]
+    post_data = urlencode(post_dict)
+    # post_data = 'method=next&params=%7B%22topic_id%22%3A253%2C%22offset%22%3A40%2C%22hash_id%22%3A%22dced108689287057f5cc3b5e85cb8289%22%7D&_xsrf=dacc17fefe1dd92f1f814fb77d3a359f'
+    return post_data
