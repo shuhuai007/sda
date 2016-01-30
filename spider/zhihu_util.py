e5a4ae135c1f63440be97df42a3e1e88e9553f58
Jie Zhou <zhoujie338@126.com>
2016-01-31 01:42:31 +0800

    refactor the code

--- spider/zhihu_util.py
+++ spider/zhihu_util.py
@@ -6,7 +6,6 @@ import gzip
 import StringIO
 import ConfigParser
 
-from urllib import urlencode
 from zhihu_constants import *
 
 REFER_DICT = {
@@ -14,20 +13,13 @@ REFER_DICT = {
     LEVEL2_TOPICS_URL : 'https://www.zhihu.com/topics'
 }
 
-def get_content(toUrl):
+def get_content(to_url):
     cookie = get_cookie()
 
-    headers = {
-        'Cookie': cookie,
-        'Host':'www.zhihu.com',
-        'Referer':REFER_DICT[toUrl],
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-        'Accept-Encoding':'gzip'
-    }
-    headers = get_headers(cookie, toUrl)
+    headers = get_headers(cookie, to_url)
 
     req = urllib2.Request(
-        url = toUrl,
+        url = to_url,
         headers = headers
     )
 
@@ -41,7 +33,7 @@ def get_content(toUrl):
         content = page.read()
     except Exception,e:
         # if count % 1 == 0:
-        #     print str(count) + ", Error: " + str(e) + " URL: " + toUrl
+        #     print str(count) + ", Error: " + str(e) + " URL: " + to_url
         return "FAIL"
 
     if page.info().get('Content-Encoding') == 'gzip':
@@ -52,18 +44,13 @@ def get_content(toUrl):
 
     return content
 
-def post(toUrl, post_data):
+def post(to_url, post_data):
     cookie = get_cookie()
 
-    headers = {
-        'Cookie': cookie,
-        'Host':'www.zhihu.com',
-        'Referer':'https://www.zhihu.com/topics',
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-        'Accept-Encoding':'gzip'
-    }
+    headers = get_headers(cookie, to_url)
 
-    req = urllib2.Request(toUrl, post_data, headers)
+
+    req = urllib2.Request(to_url, post_data, headers)
     resp = urllib2.urlopen(req)
     content = resp.read()
     if resp.info().get('Content-Encoding') == 'gzip':
@@ -98,11 +85,11 @@ def get_cookie():
     cookie = cf.get("cookie", "cookie")
     return cookie
 
-def get_headers(cookie, toUrl):
+def get_headers(cookie, to_url):
     headers = {
-        'Cookie': cookie,
+        'Cookie':cookie,
         'Host':'www.zhihu.com',
-        'Referer':'http://www.zhihu.com/',
+        'Referer':REFER_DICT[to_url],
         'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
         'Accept-Encoding':'gzip'
     }
