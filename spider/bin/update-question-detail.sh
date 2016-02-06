bd8fa7f33b6760f314c3a78d6b55e3f184eca3b1
Jie Zhou <zhoujie338@126.com>
2016-02-06 18:13:02 +0800

    modfiy shell remotely

--- spider/bin/update-question-detail.sh
+++ spider/bin/update-question-detail.sh
@@ -8,6 +8,53 @@ cd ${spider_dir}
 log_dir=${spider_dir}/log
 [ ! -d ${log_dir} ] && mkdir ${log_dir}
 
+usage() {
+cat << EOF
+    usage: $0 options
+
+    NAME
+        update-question-detail.sh - script to update the question remotely.
+
+    SYNOPSIS
+        $0 -m <run-mode>
+
+    OPTIONS:
+        -h      Show this message
+        -m      running mode - develop/prod
+EOF
+}
+
+parseArgs() {
+    if [ $# -lt 1 ]; then
+        echo "Error.. need args"
+        usage
+        exit 1
+    fi
+
+    while getopts "m:h" arg
+    do
+        case ${arg} in
+             m)
+                mode=$OPTARG
+                ;;
+             h)
+                usage
+                exit 1
+                ;;
+             ?)
+                echo "Unkonw argument"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+mode=prod
+
+parseArgs $*
+echo "......mode:$mode"
+
 echo "......execute update-question-detail.sh remotely......"
 
-nohup python ${spider_dir}/zhihu_question_detail.py -m develop > ${log_dir}/question_detail.log 2>&1 &
+#nohup python ${spider_dir}/zhihu_question_detail.py -m develop > ${log_dir}/question_detail.log
+# 2>&1 &
