f4e864684b751dd22174ecff0d785b91eb7eaf5e
Jie Zhou <zhoujie338@126.com>
2016-02-06 18:52:58 +0800

    adjust max id

--- spider/zhihu_question_detail.py
+++ spider/zhihu_question_detail.py
@@ -7,7 +7,7 @@ from transaction_manager import TransactionManager
 import zhihu_question_detail_parser
 
 
-MAX_QUESTION_TABLE_ID = 500000
+MAX_QUESTION_TABLE_ID = 1000000
 QUESTION_ID_STEP = 10
 
 def generate_available_ids(max_id, step):
