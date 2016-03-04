941cd5fe0ca83bf81206444316f1b489a36097ed
Jie Zhou <zhoujie338@126.com>
2016-03-04 17:43:30 +0800

    add shell script for hadoop

--- oozie-wf/workflow/script/shell/move-data.sh
+++ oozie-wf/workflow/script/shell/move-data.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-hadoop fs -mkdir /user/flume/project/test_table
+# create dir if not exist
+hadoop fs -test -e /user/flume/project/test_table
+if [ $? != "0" ]; then
+    echo "create directory"
+    hadoop fs -mkdir /user/flume/project/test_table
+fi
+
 hadoop fs -mv /user/flume/aaa.txt /user/flume/project/test_table
