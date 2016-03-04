0132313c64dd745041f306fdffa6278a89243bae
Jie Zhou <zhoujie338@126.com>
2016-03-04 17:17:22 +0800

    add hive script for oozie

--- oozie-wf/workflow/script/shell/move-data.sh
+++ oozie-wf/workflow/script/shell/move-data.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-hadoop fs -mv /user/flume/aaa.txt /user/flume/project
+hadoop fs -mkdir /user/flume/project/test_table
+hadoop fs -mv /user/flume/aaa.txt /user/flume/project/test_table
