49c5ff9cfa2d841af7387f3e8a821998539f0cac
Jie Zhou <zhoujie338@126.com>
2016-03-24 16:49:35 +0800

    refactor the wf config

--- oozie-wf/workflow/script/shell/move-data.sh
+++ oozie-wf/workflow/script/shell/move-data.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
-FLUME_SOURCE_DIR="/user/flume/project/sda/spider/user"
-FLUME_DATA_TEMP_DIR="/user/flume/project/test_table"
+#FLUME_SOURCE_DIR="/user/flume/project/sda/spider/question"
+#FLUME_DATA_TEMP_DIR="/user/flume/project/test_question_table"
+
+
+parseArgs() {
+    while getopts "o:i:h" arg
+    do
+        case ${arg} in
+             i)
+                FLUME_SOURCE_DIR=$OPTARG
+                ;;
+             o)
+                FLUME_DATA_TEMP_DIR=$OPTARG
+                ;;
+             h)
+                usage
+                exit 1
+                ;;
+             ?)
+                echo "Unknown argument"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+parseArgs $*
 
 # create dir if not exist
 hadoop fs -test -e ${FLUME_DATA_TEMP_DIR}
